bf3394319ccb69e571340d82f57c23d62c49e8db
Fabian Johannes Hoffmann <fhoffmann.mmt-b2013@fh-salzburg.ac.at>
2015-02-06 01:28:07 +0100

    first deployment

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,29 @@
-$(function () {
-//    lib.log('Ready');
+function grayscale(src){
+
+var graycanvas = document.createElement("canvas");
+var ctx = graycanvas.getContext("2d");
+var imgObj = new Image();
+imgObj.src = 'html.png';
+graycanvas.width = imgObj.width;
+graycanvas.height = imgObj.height;
+console.log(src);
+console.log(graycanvas.height);
+ctx.drawImage(imgObj, 0, 0);
+var canvasPixels = ctx.getImageData(0, 0, 512, 512);
+
+for(var y = 0; y < 512; y++){
+for(var x = 0; x < 512; x++){
+
+var i = (y * 4) * canvasPixels.width + x * 4;
+var average = (canvasPixels.data[i] + canvasPixels.data[i + 1] + canvasPixels.data[i + 2]) / 3;
+canvasPixels.data[i] = average;
+canvasPixels.data[i + 1] = average;
+canvasPixels.data[i + 2] = average;
+}
+}
+ctx.putImageData(canvasPixels, 0, 0, 0, 0, canvasPixels.width, canvasPixels.height);
+return graycanvas .toDataURL();
+}
 
 
-});
+//usage $(selector).grayScale();
