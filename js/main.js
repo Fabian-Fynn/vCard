6fdce66cc968cdf9436fe98bb79e61e5590e3ccd
Fabian Johannes Hoffmann <fhoffmann.mmt-b2013@fh-salzburg.ac.at>
2015-04-18 01:28:46 +0200

    feat badges randomized

--- js/main.js
+++ js/main.js
@@ -12,10 +12,12 @@ $(function() {
   var badgeTitle
 
   $(".badge").on("mouseenter", function(ev){
-    var $elem = $(this)
-    badgeTitle = $elem.attr("title")
-    $elem.removeAttr("title")
-    $elem.append(badgeTitle)
+    if($(window).width() > 1200){
+      var $elem = $(this)
+      badgeTitle = $elem.attr("title")
+      $elem.removeAttr("title")
+      $elem.append(badgeTitle)
+    }
   })
 
   $(".badge").on("mouseleave", function(){
@@ -36,32 +38,33 @@ function parallax(){
 }
 
 function createBadges(){
-  var badges = {
-                  html: "HTML5",
-                  css: "CSS3",
-                  sass: "SASS/SCSS",
-                  js: "JavaScript",
-                  jquery: "jQuery",
-                  angular: "AngularJS",
-                  node: "NodeJS",
-                  rails: "Ruby on Rails 4",
-                  php: "PHP",
-                  mongodb: "mongoDB",
-                  postgres: "PostgreSQL",
-                  mysql: "MYSQL",
-                  git: "Git",
-                  bootstrap: "Twitter's Bootstrap",
-                  wordpress: "Wordpress",
-                  cs: "Visual C#",
-                  ios: "iOS - Swift",
-                  ps: "Adobe Photoshop CC",
-                  pr: "Adobe Premiere Pro CC",
-                  ae: "Adobe After Effects CC"
-              }
   $("#badges").append('<div class="col"></div>')
+  var badges = [
+                  ['html', "HTML5"],
+                  ['css', "CSS3"],
+                  ['sass', "SASS/SCSS"],
+                  ['js', "JavaScript"],
+                  ['jquery', "jQuery"],
+                  ['angular', "AngularJS"],
+                  ['node', "NodeJS"],
+                  ['rails', "Ruby on Rails 4"],
+                  ['php', "PHP"],
+                  ['mongodb', "mongoDB"],
+                  ['postgres', "PostgreSQL"],
+                  ['mysql', "MYSQL"],
+                  ['git', "Git"],
+                  ['bootstrap', "Twitter's Bootstrap"],
+                  ['wordpress', "Wordpress"],
+                  ['cs', "Visual C#"],
+                  ['ios', "iOS - Swift"],
+                  ['ps', "Adobe Photoshop CC"],
+                  ['pr', "Adobe Premiere Pro CC"],
+                  ['ae', "Adobe After Effects CC"]
+              ]
+  badges = _.shuffle(badges)
   $.each(badges, function( key, value ){
-    $("#badges .col").append('<div class="badge" id="badge_' + key + '" title="' + value + '" ></div>')
-    $("#badge_" + key).css('background-image', "url(assets/img/" + key + ".png)")
+    $("#badges .col").append('<div class="badge" id="badge_' + value[0] + '" title="' + value[1] + '" ></div>')
+    $("#badge_" + value[0]).css('background-image', "url(assets/img/" + value[0] + ".png)")
   })
 
 }
